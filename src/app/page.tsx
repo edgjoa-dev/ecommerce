2227eec25bbcf5f94e377fcfb9ef82810f09d4be
Edgar Joaquin Flores <floresedgarjoaquin@gmail.com>
2025-09-01 21:50:46 -0600

    Config: Se realizan las configuraciones para estilos con tailwind + css, en archivo globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-const HomePage = () => {
+export default function HomePage() {
   return (
-    <div className="d-flex flex-col items-center justify-center content-center min-h-screen w-screen p-4 bg-black text-white">
-      <h1>Welcome to the Home Page</h1>
-      <h3>This is the main content of the home page.</h3>
-      <p>This is the home page of our application.</p>
-    </div>
+    <main className="flex flex-col justify-start items-start bg-black text-white w-screen h-screen p-4">
+      <section className="flex justify-center items-start w-screen h-screen">
+        <h1>Titulo</h1>
+        <p>este es mi primer ecommerce en nextjs</p>
+      </section>
+    </main>
   );
 }
-export default HomePage;
